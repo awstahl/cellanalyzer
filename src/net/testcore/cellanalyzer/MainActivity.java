d860512e9f0778b27c2841d86dcd4c4bd75c0b53
awstahl <alex@testcore.net>
2014-12-07 00:15:21 -0800

    Added refresh functionality

--- src/net/testcore/cellanalyzer/MainActivity.java
+++ src/net/testcore/cellanalyzer/MainActivity.java
@@ -1,11 +1,10 @@
 package net.testcore.cellanalyzer;
 
 import java.util.List;
-
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Context;
 import android.content.res.Resources;
-//import android.content.Intent;
 import android.os.Bundle;
 import android.telephony.CellInfo;
 import android.telephony.NeighboringCellInfo;
@@ -13,8 +12,6 @@ import android.telephony.TelephonyManager;
 import android.util.SparseArray;
 import android.view.Menu;
 import android.view.MenuItem;
-//import android.view.View;
-//import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
@@ -39,21 +36,20 @@ public class MainActivity extends Activity {
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
+
+		switch(item.getItemId()) {
+		case R.id.action_refresh:
+			writeViews();
 			return true;
+		default:
+			return super.onOptionsItemSelected(item);
 		}
-		return super.onOptionsItemSelected(item);
 	}
 }
 
